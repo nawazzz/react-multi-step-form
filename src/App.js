1ea6e0dcc123cb44c5aa3ffef3393e0e1cdfec9a
unknown <khan.nawaz790@gmail.com>
2022-03-31 10:19:01 +0200

    create multi step form project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="mainContainer">
+        <div className='subMainContainer'>
+          <h1>React Multi Step Form</h1>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
